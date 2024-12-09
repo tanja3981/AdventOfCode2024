c3b2523a35675f0f7c411928be1c04e257ea81e6
Tanja Schmidt <schreibtanja@gmail.com>
2024-12-09 11:50:13 +0100

    2024/12/07: Task 1

--- src/main/java/de/tanschmi/aoc2024/dec7/Dec7.java
+++ src/main/java/de/tanschmi/aoc2024/dec7/Dec7.java
@@ -1,8 +1,10 @@
 package de.tanschmi.aoc2024.dec7;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
+import net.objecthunter.exp4j.operator.Operator;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -10,16 +12,26 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
+import java.util.function.Function;
 
 import static de.tanschmi.aoc2024.NumberUtil.parseToLong;
+import static net.objecthunter.exp4j.operator.Operator.PRECEDENCE_ADDITION;
 
 @Slf4j
 public class Dec7 {
 
+    @SneakyThrows
     long task1(File file) throws IOException, Dec7Exception {
         String s = FileUtils.readFileToString(file, Charset.defaultCharset());
         return task1(s);
+    }
+
+    @SneakyThrows
+    long task2(File file) throws IOException, Dec7Exception {
+        String s = FileUtils.readFileToString(file, Charset.defaultCharset());
+        return task2(s);
     }
 
     long task1(String text) throws Dec7Exception {
@@ -27,7 +39,7 @@ public class Dec7 {
         List<String> lines = text.lines().toList();
         long validSum = 0;
         for (String line : lines) {
-            long sum = testLine(line);
+            long sum = testLine(line, Dec7::collectExressionsForTask1);
             if (sum != -1) {
                 validSum += sum;
             }
@@ -35,7 +47,20 @@ public class Dec7 {
         return validSum;
     }
 
-    long testLine(String line) throws Dec7Exception {
+    long task2(String text) throws Dec7Exception {
+
+        List<String> lines = text.lines().toList();
+        long validSum = 0;
+        for (String line : lines) {
+            long sum = testLine(line, Dec7::collectExpressionsForTask2);
+            if (sum != -1) {
+                validSum += sum;
+            }
+        }
+        return validSum;
+    }
+
+    long testLine(String line, Function<String[], List<String>> collectExpressions) throws Dec7Exception {
         String[] args = StringUtils.split(line, ":");
         if (args.length != 2) {
             throw new Dec7Exception("Ungültige Zeile " + line + " wurde in " + args + " gesplittet");
@@ -45,29 +70,59 @@ public class Dec7 {
 
         String[] operatorsStr = StringUtils.split(args[1].trim(), " ");
 
-        List<String> calcs = collectExressions(operatorsStr);
+        List<String> calcs = collectExpressions.apply(operatorsStr);
+        log.info("{}: Calculating {} different possibilities!", line, calcs.size());
 
         for (String calculation : calcs) {
             if (testCalculation(calculation, testValue)) {
                 return testValue;
             }
         }
-       // log.info("no valid calculation for line {}", line);
         return -1;
     }
 
-    private boolean testCalculation(String calculation, long expected) {
-        Expression expr = new ExpressionBuilder(calculation)
-                .build();
-        long result = Double.valueOf(expr.evaluate()).longValue();
-        if (expected == result) {
-            log.debug("Valid calc: {}", calculation);
-            return true;
+    public boolean testCalculation(String totalCalculation, long expected) {
+        String restCalc = totalCalculation.toString();
+
+        while (restCalc.contains("||")) {
+            int index = restCalc.indexOf("||");
+            String left = restCalc.substring(0, index).trim();
+            String right = restCalc.substring(index + 2).trim();
+
+            boolean validLeft = false;
+            int countRemovedPara = 0;
+            while (!validLeft) {
+                try {
+                    Expression expr = new ExpressionBuilder(left)
+                            //.operator(operator)
+                            .build();
+                    long result = Double.valueOf(expr.evaluate()).longValue();
+
+                    restCalc = StringUtils.repeat('(', countRemovedPara) + Long.toString(result) + "" + right;
+                    validLeft = true;
+                } catch (IllegalArgumentException e) {
+                    validLeft = false;
+                    left = left.substring(1);
+                    countRemovedPara++;
+                }
+            }
         }
-        return false;
+        try {
+            Expression expr = new ExpressionBuilder(restCalc)
+                    //.operator(operator)
+                    .build();
+            long result = Double.valueOf(expr.evaluate()).longValue();
+            if (result == expected) {
+                //log.debug("Valid calc: {}", calculation);
+                return true;
+            }
+            return false;
+        } catch (EmptyStackException e) {
+            throw e;
+        }
     }
 
-    private List<String> collectExressions(String[] operatorsStr) {
+    public static List<String> collectExressionsForTask1(String[] operatorsStr) {
         List<String> calcs = new ArrayList<>();
         calcs.add(operatorsStr[0]);
 
@@ -77,6 +132,23 @@ public class Dec7 {
             for (String previous : calcs) {
                 newCalcs.add("(" + previous + ") + " + current);
                 newCalcs.add("(" + previous + ") * " + current);
+            }
+            calcs = newCalcs;
+        }
+        return calcs;
+    }
+
+    public static List<String> collectExpressionsForTask2(String[] operatorsStr) {
+        List<String> calcs = new ArrayList<>();
+        calcs.add(operatorsStr[0]);
+
+        for (int i = 1; i < operatorsStr.length; i++) {
+            String current = operatorsStr[i];
+            ArrayList<String> newCalcs = new ArrayList<>();
+            for (String previous : calcs) {
+                newCalcs.add("(" + previous + ") + " + current);
+                newCalcs.add("(" + previous + ") * " + current);
+                newCalcs.add("(" + previous + ") || " + current);
             }
             calcs = newCalcs;
         }
--- src/test/java/de/tanschmi/aoc2024/dec7/Dec7Test.java
+++ src/test/java/de/tanschmi/aoc2024/dec7/Dec7Test.java
@@ -1,6 +1,7 @@
 package de.tanschmi.aoc2024.dec7;
 
 import de.tanschmi.aoc2024.dec6.Dec6Task1;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -36,5 +37,39 @@ class Dec7Test {
         assertEquals(3749L, result);
     }
 
+    @Test
+    void task2_exampleText() throws Dec7Exception {
+        final String example = """
+                190: 10 19
+                3267: 81 40 27
+                83: 17 5
+                156: 15 6
+                7290: 6 8 6 15
+                161011: 16 10 13
+                192: 17 8 14
+                21037: 9 7 18 13
+                292: 11 6 16 20""";
 
+        Dec7 dec7 = new Dec7();
+        long result = dec7.task2(example);
+        assertEquals(11387L, result);
+    }
+
+    //@Test
+    //TODO: this test takes 10 hours...
+    void task2() throws Exception {
+        File inFile = new File(ClassLoader.getSystemResource("inputs/dec7.txt").getFile());
+
+        Dec7 dec6 = new Dec7();
+        long result = dec6.task2(inFile);
+
+        assertEquals(165278151522644L, result);
+    }
+
+    @Test
+    void testCalculation() {
+        Dec7 dec7 = new Dec7();
+        boolean flag = dec7.testCalculation("((((((((3) + 4) + 994) + 5) + 92) + 1) || 227) + 79) + 4", 249329);
+        assertFalse(flag);
+    }
 }
