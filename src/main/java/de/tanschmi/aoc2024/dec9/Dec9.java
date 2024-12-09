0f786bd4d84d62e31eaa589e2a894262d192ca2f
Tanja Schmidt <schreibtanja@gmail.com>
2024-12-09 19:18:47 +0100

    WIP

---
+++ src/main/java/de/tanschmi/aoc2024/dec9/Dec9.java
@@ -0,0 +1,29 @@
+package de.tanschmi.aoc2024.dec9;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+
+@Slf4j
+public class Dec9 {
+
+    int task1(File input) {
+
+    }
+
+    int task1(String input) {
+
+        String disk = getDisc(input);
+
+        return 0;
+    }
+
+    String getDisc(String input) {
+        char[] chars = input.toCharArray();
+        StringBuilder disk = new StringBuilder();
+
+
+
+    }
+}
---
+++ src/test/java/de/tanschmi/aoc2024/dec9/Dec9Test.java
@@ -0,0 +1,16 @@
+package de.tanschmi.aoc2024.dec9;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Dec9Test {
+final private Dec9 service = new dec9
+    @Test
+    void task1() {
+        String input = """
+                2333133121414131402""";
+
+
+    }
+}
